921b22d69cb0644e37bf7d29df4dff63bb5276f1
piotter121 <defcon1994@gmail.com>
2015-06-09 18:59:19 +0200

    Początki

---
+++ stronghold.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# wszystkie importy
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+# konfiguracja
+DATABASE = 'database/Stronghold.db'
+SECRET_KEY = 'development.key'
+SERVER_MAIL = 'pyskp@ee.pw.edu.pl'
+SERVER_PASS = 'pyskp12345'
+
+# tworzenie aplikacji
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db:
+	return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == "__main__":
+	app.run()
